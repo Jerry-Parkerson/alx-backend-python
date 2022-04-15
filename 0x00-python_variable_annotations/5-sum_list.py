cf7bc2676b40165b0081a83d4043d5afbb8bbded
Paul jerry <paulomoragbon750@gmail.com>
2022-04-14 21:00:18 -0700

    sum

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" list of floats """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ function sum_list of floats """
+    if input_list is none:
+        return 0
+    else:
+        return sum(input_list)
