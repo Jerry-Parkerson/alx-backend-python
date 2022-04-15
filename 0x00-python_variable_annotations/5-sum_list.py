b39acf1ca84e09a526e832a9aa06e286bdd48e79
Paul jerry <paulomoragbon750@gmail.com>
2022-04-14 21:01:40 -0700

    sum

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-""" list of floats """
+"""Contains a function that sums a list of floats"""
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    """ function sum_list of floats """
-    if input_list is none:
+    """Sums a list of floats
+    Args:
+        input_list (list): A list of floats
+    Returns:
+        float: The sum of the floats in the list
+    """
+    if input_list is None:
         return 0
     else:
         return sum(input_list)
